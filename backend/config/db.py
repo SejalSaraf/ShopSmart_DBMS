519eee247a7928c134d36cfe2918e85039a54324
Mira <mira.vadjikar@cumminscollege.in>
2025-10-07 21:51:13 +0530

    Update db.py

--- backend/config/db.py
+++ backend/config/db.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Komal@2005",   # Your MySQL password
+        password="strawberryshortcake",   # Your MySQL password
         database="OnlineShoppingDB"
     )
